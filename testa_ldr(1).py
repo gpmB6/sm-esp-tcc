48584a3495fa1b8f158d7ec1130a5ebd72772ff6
gbpm <gabi8b@hotmail.com>
2023-11-09 16:09:00 -0300

    Delete testa_ldr(1).py

--- testa_ldr(1).py
+++
@@ -1,1 +0,0 @@
-# Testa se o sensor está funcional ## Sensor (LDR):# GND1, GPIO16(D0) e A0 (compartilhada com o potenciometro)from machine import Pin, ADCfrom time import sleepsens = Pin(16, Pin.OUT)analog = ADC(0)while True:  pot.off()  sens.on()  analog_value = analog.read()  print(analog_value)  sleep(1)
