72c9551a20fa63f57925a51ba87bc9f4a9792ebd
gbpm <gabi8b@hotmail.com>
2023-11-30 11:02:09 -0300

    Update secrets.py

--- secrets.py
+++ secrets.py
@@ -3,8 +3,11 @@
 #SSID = 'nomde_da_rede_aqui'
 #SENHA = 'senha_da_rede_aqui'
 
-#API DO IFTTT
-#api_key = 'sua_api_aqui'
+#api_key = 'sua_api_do_IFTTT_aqui'
 
-#API DO SHEETSU 
-#url = 'sua_url_aqui'
+#API_KEY DO PROJETO
+api_key = 'fj6vQD1kaPXtjaLN1cutzdMTiYHFVelpoGRR-FKZDet'
+
+#URL DO PROJETO
+#url = 'sua_url_do_SHEETSU_aqui'
+url = 'https://sheetsu.com/apis/v1.0su/52d37fb77dc7'
