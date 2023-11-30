29e9dd070307dbb0aba2edcb855c667830ce2728
gbpm <gabi8b@hotmail.com>
2023-11-30 15:53:10 -0300

    Update boot.py

--- boot.py
+++ boot.py
@@ -1,19 +1,28 @@
-
-import uos, machine
-import sys
+#Funções relacionadas ao sistema operacional, manipulação de arquivos / diretorios
+import uos
+#Leitura e gravacao de arquivos
 import os
+#Funções para interagir com o hardware, como pinos GPIO
+import machine
+#Possibilita acessar variaveis / funcoes do interpretador
+import sys
+#Funcoes relacionadas ao ESP, como controle do modo de depuracao (debug)
 import esp
+#Funcoes com tempo
 import time
+#Conversoes entre binarios e ascii
 import ubinascii
 esp.osdebug(None)
-import network, webrepl, secrets
-import urequests
+#Ativa a rede, o WebREPL, o arquivo com chaves e a biblioteca externa UREQUESTS para manipular URLs
+import network, webrepl, secrets, urequests
+#Controle de coleta de lixo
 import gc
 gc.collect()
 
+#Conecta a rede
 station = network.WLAN(network.STA_IF)
 station.active(True)
-station.connect(secrets.ssid, secrets.password)
+station.connect(secrets.ssid, secrets.senha)
 
 while station.isconnected() == False:
   pass
