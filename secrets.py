9346ff871d6b817477a6a0e8adce75fa851109ed
gbpm <gabi8b@hotmail.com>
2023-11-30 10:58:43 -0300

    Update secrets.py

--- secrets.py
+++ secrets.py
@@ -1,4 +1,10 @@
+#DEPOIS DE EDITAR OS PARAMETROS, DESCOMENTE A LINHA CORRESPONDENTE
+
 #SSID = 'nomde_da_rede_aqui'
 #SENHA = 'senha_da_rede_aqui'
 
+#API DO IFTTT
 #api_key = 'sua_api_aqui'
+
+#API DO SHEETSU 
+#url = 'sua_url_aqui'
